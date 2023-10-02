ceaf6a45aabc8e00108bb12a0088c727d431503f
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-10-01 20:00:55 -0500

    Overhauled date system
    rebuilt date system, to be more readable and performant. In the process also fixes indexing and month sizes.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ function App() {
   const [csvData, setCsvData] = useState([]);
   const [selectedColumn, setSelectedColumn] = useState('');
   const [sizes, setSizes] = useState([]);
-  const [dates, setDate] = useState(new Date());
+  const [dates, setDates] = useState([]);
   const [usedDate, setUsedDate] = useState(new Date());
-  const [startDateIndex, setstartDateIndex] = useState(0);
-  const [endDateIndex, setendDateIndex] = useState(0);
   // get the start day of the month as an int (0 - 6)
   const [usedFirstDayofMonth, setusedFirstDayofMonth] = useState(0);
-
-  let notFullMonth = false;
+  const [dateSizeDict, setdateSizeDict] = useState({}); // dict of dates and sizes
 
   const handleCsvDataChange = (data) => {
     setCsvData(data);
@@ -23,72 +20,66 @@ function App() {
     setSelectedColumn(e.target.value);
   };
 
-  function setInitialDate() {
-    if (dates.length > 0) {
-      setUsedDate(new Date(dates[0]));
-      setstartDateIndex(usedDate.getDate());
-      setendDateIndex(getDaysInMonth(usedDate.getFullYear(), usedDate.getMonth()));
-      setusedFirstDayofMonth(getFirstDayOfWeek(usedDate.getFullYear(), usedDate.getMonth()) + usedDate.getDate());
-    }
-    else {
-      console.log("dates len" + dates.length);
-    }
-  }
-
   useEffect(() => {
     // Use the map function to extract the selected column values
     const extractedSizes = csvData.map((row) => parseFloat(row[selectedColumn]));
-    const extractedDates = csvData.map((row) => row['Date']);
+    let extractedDates = csvData.map((row) => row['Date']);
 
     // Filter out NaN values (non-numeric) and set the sizes array
     setSizes(extractedSizes.filter((value) => !isNaN(value)));
-    // Filter out NaN values (non-string) and set the sizes array
-    setDate(extractedDates.filter((value) => !isNaN(value)));
     // extract the dates using date.parse
-    setDate(extractedDates.map((value) => Date.parse(value)));
-    setInitialDate();
+    extractedDates = extractedDates.map((value) => new Date(Date.parse(value)));
+    // Filter out NaN values (non-string) and set the sizes array
+    extractedDates = extractedDates.filter((value) => !isNaN(value));
+    // make sure the dates are in the correct format
+    extractedDates = extractedDates.map((value) => new Date(value.getFullYear(),
+      value.getMonth(), value.getDate() + 1 /*appears to be 0 indexed*/));
+
+    setDates(extractedDates);
+
+    let newDict = {};
+
+    for (let i = 0; i < extractedDates.length; i++) {
+      newDict[extractedDates[i]] = extractedSizes[i];
+    }
+
+    setdateSizeDict(newDict);
+    usedDate.setMonth(usedDate.getMonth() - 1);
   }, [csvData, selectedColumn]);
 
-  function getDaysInMonth(year, month) {
-    const lastDayOfMonth = new Date(year, month + 1, 0);
-    return lastDayOfMonth.getDate();
-  }
   function getFirstDayOfWeek(year, month) {
     const firstDayOfMonth = new Date(year, month, 1);
     return firstDayOfMonth.getDay();
   }
 
-  // function to go to the next month
-  // there is a problem with this function
-  // 
-
   const nextMonth = () => {
-    if (startDateIndex === 0) {
-      setInitialDate();
-      notFullMonth = true;
-    }
-    else {
-      notFullMonth = false;
-    }
-    setstartDateIndex(endDateIndex + 1);
     setUsedDate(new Date(usedDate.getFullYear(), usedDate.getMonth() + 1, 1));
-    setendDateIndex(endDateIndex + getDaysInMonth(usedDate.getFullYear(), usedDate.getMonth())); // get the length of the next month and add it to the end index
     setusedFirstDayofMonth(getFirstDayOfWeek(usedDate.getFullYear(), usedDate.getMonth()));
-    if (notFullMonth) {
-      setusedFirstDayofMonth(usedFirstDayofMonth + dates[0].getDate())
-    }
   }
 
   // function to go to the previous month
   const previousMonth = () => {
-    if (startDateIndex === 0) {
-      setInitialDate();
-      return;
-    }
-    setstartDateIndex(startDateIndex - getDaysInMonth(usedDate.getFullYear(), usedDate.getMonth() - 1)); // get the length of the previous month
     setUsedDate(new Date(usedDate.getFullYear(), usedDate.getMonth() - 1, 1));
-    setendDateIndex(endDateIndex - getDaysInMonth(usedDate.getFullYear(), usedDate.getMonth() - 1)); // get the length of the next month and add it to the end index
     setusedFirstDayofMonth(getFirstDayOfWeek(usedDate.getFullYear(), usedDate.getMonth()));
+  }
+
+  const getSizesByMonth = (year, month) => {
+    //const datesToBeIncluded = dates.filter((date) => date.getMonth() === month);
+    let datesToBeIncluded = [...Array(32).keys()].map((day) => new Date(year, month, day));
+    datesToBeIncluded = datesToBeIncluded.filter((date) => date.getMonth() === month);
+    const unFiliteredMonthSizes = datesToBeIncluded.map((date) => dateSizeDict[date]);
+    // filter out undefined values
+    const monthSizes = unFiliteredMonthSizes.filter((value) => value !== undefined);
+    return monthSizes;
+  }
+
+  const getSizeByDay = (day) => {
+    const fullDate = new Date(usedDate.getFullYear(), usedDate.getMonth(), day);
+    let size = dateSizeDict[fullDate];
+    if (size === undefined || fullDate.getMonth() !== usedDate.getMonth()) {
+      return 0;
+    }
+    return size;
   }
 
   return (
@@ -120,21 +111,20 @@ function App() {
             ></span>
           </div>
         ))}
-        {sizes.slice(startDateIndex, endDateIndex + 1 /*not inclusive*/).map((size, index) => (
-          <div className="center" key={[size, index]}>
+        {[...Array(32).keys()].map((day) => ( // add empty divs to the beginning of the array to make the calendar start on the correct day
+          <div className="center">
             <div className="dotText">
-              {index + 1}</div>
+              {(getSizeByDay(day + 1) != 0) ? day + 1 : ''}</div>
             <span
               className="dot"
               style={{
-                height: ((size / Math.max(...sizes.slice(startDateIndex, endDateIndex + 1 /*not inclusive*/))) * 60 + 'px'),
-                width: ((size / Math.max(...sizes.slice(startDateIndex, endDateIndex + 1 /*not inclusive*/))) * 60 + 'px'),
+                height: getSizeByDay(day + 1) / Math.max(...getSizesByMonth(usedDate.getFullYear(), usedDate.getMonth())) * 60 + 'px',
+                width: getSizeByDay(day + 1) / Math.max(...getSizesByMonth(usedDate.getFullYear(), usedDate.getMonth())) * 60 + 'px',
                 zIndex: -1
               }}
             ></span>
           </div>
         ))}
-
       </div>
       <div>
         <div className="previousButton" onClick={previousMonth}>previous</div>
